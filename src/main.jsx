d32f620210de2e802d901c97db655bcbe00c464a
siddhanttyagi10 <siddhant@growth.cx>
2024-03-10 11:46:21 +0530

    pages-maded-succesfully

---
+++ src/main.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Dashboard from './components/Dashboard/Dashboard.jsx'
+import ProductManagement from './components/ProductManagement/ProductManagement.jsx'
+import OrderManagement from './components/OrderManagement/OrderManagement.jsx'
+import User from './components/User/User.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout/>,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />
+//       },
+//       {
+//         path: "about",
+//         element: <About />
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />
+//       }
+//     ]
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Dashboard />} />
+      <Route path='product-management' element={<ProductManagement />} />
+      <Route path='order-management' element={<OrderManagement />} />
+      <Route path='user/:userid' element={<User />} />
+      
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
