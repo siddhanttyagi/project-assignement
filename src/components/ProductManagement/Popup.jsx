809092d1cf2b358c3c7f8a77b41fb0501726626b
siddhanttyagi10 <siddhant@growth.cx>
2024-03-14 13:51:46 +0530

    pop-css-done

--- src/components/ProductManagement/Popup.jsx
+++ src/components/ProductManagement/Popup.jsx
@@ -5,11 +5,11 @@ import './Popup.css';
 const Popup = ({ onClose, popupObject }) => {
   return (
     <div className="popup-container">
-      <div className="popup">
+      <div className="popup relative">
         <h2 className='text-black'>Popup Content</h2>
         <h2 className='text-black'>{popupObject.title}</h2>
         <p className='text-black'>This is the content of the popup.</p>
-        <button className='text-black' onClick={onClose}>Close</button>
+        <button className='text-red-600 absolute right-1 top-0 text-2xl font-bold' onClick={onClose}>X</button>
       </div>
     </div>
   );
