69c9e4c720a2618e2e7e7f8410840e8918daea59
siddhanttyagi10 <siddhant@growth.cx>
2024-03-15 20:10:58 +0530

    final-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,12 +6,9 @@ import Header from './components/Header/Header'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <>
-     
-      
+    <>    
     </>
   )
 }
